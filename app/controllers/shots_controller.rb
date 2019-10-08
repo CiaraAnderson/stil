3008df1698704f7f68a3fcc5b4aa0bebb4c2cccc
Ciara Anderson <thelaylinguist@gmail.com>
2019-10-08 19:32:06 +0000

    Added Simple Form to upload shots

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,4 +1,20 @@
 class ShotsController < ApplicationController
+
+  def new
+    @shot = Shot.new
+  end
+
   def index
+  end
+
+  def create
+    @shot = Shot.create(shot_params)
+    redirect_to root_path
+  end
+
+  private 
+
+  def shot_params
+   params.require(:shot).permit(:message)
   end
 end
--- spec/controllers/shots_controller_spec.rb
+++ spec/controllers/shots_controller_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe ShotsController, type: :controller do
 
   end
  end
+
+
+ describe "shots#new action" do 
+   it "should successfully show the new form" do 
+      get :new
+      expect(response).to have_http_status(:success)
+
+   end
+ end
+
+ describe "shots#create action" do
+  it "should successfully create a new shot in our database" do 
+    post :create, params: { shot: { message: 'Hello!' } }
+    expect(response).to redirect_to root_path
+
+    shot = Shot.last
+    expect(shot.message).to eq("Hello!")
+  end
+ end
 end
